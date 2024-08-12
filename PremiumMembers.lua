09b920ce1b59dc9e6303ebd8750819d1a9057ef7
OMOHOTA <OMOHOTA@GMAIL.COM>
2024-08-12 20:35:58 +0700

    Create PremiumMembers.lua

---
+++ PremiumMembers.lua
@@ -0,0 +1,18 @@
+local allowedPlayers = {
+    "hotomoma",
+    "ikdkd130",
+    "ikdkd128",
+    "xkaiden30x",
+    "calamitynoteL",
+    "prismaticofchaos",
+    "calamitynotem",
+    "jibuhms585",
+    "bruhboy19079",
+    "lackyiy2",
+    "iloveslapbattlezxd47",
+    "lackyiy",
+    "fiufid8e8eueuejeueu",
+    "banana689001",
+    "anormalpersonorisit",
+    "robbobertsonthethird"
+}
